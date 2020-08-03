6e3f32a3d25e97007f59768e79883b2809e3aba7
Jason Climo <jason@Jasons-MacBook-Pro.local>
2020-08-04 09:34:27 +1200

    Added the data functions

--- src/js/custom.js
+++ src/js/custom.js
@@ -15,49 +15,81 @@ var axios = require('axios');
 		}
 	}) // Vue ENDS
 
+	function axiosRequest () {
+		axios({
+		  method: 'get',
+		  url: 'http://newsapi.org/v2/top-headlines?country=nz&category=business&apiKey=e156c57afb23498ea5b1404034a6e785'
+		})
+		  .then(function (response) {
+		    // console.log(response)
+				app.newsData = response;
+				console.log(app.newsData);
+				console.log('jdsjkadshjkadshjkads');
 
-	// Making the axios request
-	// var url = 'http://newsapi.org/v2/top-headlines?' +
-	//           'country=us&' +
-	//           'apiKey=7a29e827b9d546e5b54416430ce0fc27';
-	// var req = new Request(url);
-	// fetch(req)
-	//     .then(function(response) {
-	//         // console.log(response.json());
-	// 				app.weatherData = response.json();
-	// 				console.log(app.response);
-	//     })
+				getImage();
+				getTitle();
+				getDesc();
+		  }); // Request ENDS
+	}
+	// Making an axios request
 
-		// GET request for remote image in node.js
 
-	axios({
-	  method: 'get',
-	  url: 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e156c57afb23498ea5b1404034a6e785'
-	  // responseType: 'stream'
-	})
-	  .then(function (response) {
-	    // console.log(response)
-			app.newsData = response
-			console.log(app.newsData)
-			console.log('dsdsdasdasdas')
-	  });
+
 
 	// getImage STARTS
 	function getImage () {
-		console.log('function works')
-		for (var i = 0; i < 20; i++) {
+		console.log('function works AGAIN');
+		var imageArray = [ ]
+		for (var i = 0; i < app.newsData.data.articles.length; i++) {
 			console.log(i)
 
-			app.image = {
-				i: app.data.articles[i].urlToImage
-			}
+
+			imageArray.push(app.newsData.data.articles[i].urlToImage)
+
+
 		}
+		// console.log(imageArray + "this is arasdasdas")
+		app.image = imageArray;
 		// testing to see if the data is there
-		console.log(app.image)
-	} // GetIMAGE ENDS
+		console.log(app.image);
+	}; // GetIMAGE ENDS
 
 
+	// getTitle STARTS
+	function getTitle () {
+		console.log('function works AGAIN');
+		var titleArray = [ ]
+		for (var i = 0; i < app.newsData.data.articles.length; i++) {
+			console.log(i)
 
+
+			titleArray.push(app.newsData.data.articles[i].title)
+
+
+		}
+		// console.log(imageArray + "this is arasdasdas")
+		app.title = titleArray;
+		// testing to see if the data is there
+		console.log(app.title);
+	}; // GetTitle ENDS
+
+
+	// getDesc STARTS
+	function getDesc () {
+		console.log('function works AGAIN');
+		var descriptionArray = [ ]
+		for (var i = 0; i < app.newsData.data.articles.length; i++) {
+			console.log(i)
+
+
+			descriptionArray.push(app.newsData.data.articles[i].description)
+
+
+		}
+		app.description = descriptionArray;
+		// testing to see if the data is there
+		console.log(app.description);
+	}; // getDesc ENDS
 
 
 
@@ -66,6 +98,7 @@ var axios = require('axios');
 
 
 	// Calling the functions
-	// getImage()
+	axiosRequest()
+
 
 })(); // iffe ENDS
